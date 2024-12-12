e588fc91d8a3f1220e04a8a7f06618028124cbf6
George Bartram <george.bartram29@gmail.com>
2024-12-12 16:15:49 +0000

    adds create event page with form

--- src/app/create-event/create-event-form/create-event-form.component.ts
+++ src/app/create-event/create-event-form/create-event-form.component.ts
@@ -1,20 +1,79 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, Output, signal, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatHint, MatInputModule } from '@angular/material/input'; 
+import { MatSelectModule } from '@angular/material/select'; 
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { TextFieldModule } from '@angular/cdk/text-field'; 
+import { endAfterStartValidator } from '../../_shared/validators/end-after-start.directive';
+import { futureStartValidator } from '../../_shared/validators/future-start.directive';
+import { EventsService } from '../../services/events/events.service';
+import { Observable } from 'rxjs';
+import iso3166_1 from "iso-3166-1"
+import iso3166_2 from "iso-3166-2"
+import { KeyValuePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-create-event-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatInputModule, TextFieldModule, MatSelectModule, MatCheckboxModule, MatButtonModule, MatHint, KeyValuePipe],
   templateUrl: './create-event-form.component.html',
   styleUrl: './create-event-form.component.css'
 })
 export class CreateEventFormComponent {
+  constructor(private eventsService: EventsService) {}
+  timeZones = Intl.supportedValuesOf("timeZone");
+  currencies = Intl.supportedValuesOf("currency");
+  countries = iso3166_1.all()
+  defaultCountryCode = "GB"
+  defaultCurrencyCode = "GBP"
+
   eventForm = new FormGroup({
-    name: new FormControl(""),
-    timezone: new FormControl(""),
-    startDate: new FormControl(""),
-    startTime: new FormControl(""),
-    endDate: new FormControl(""),
-    endTime: new FormControl(""),
-    currency: new FormControl("")
-  })
+      name: new FormControl("", [Validators.required, Validators.pattern(/^\S.*/)]),
+      timeZone: new FormControl("UTC", Validators.required),
+      start: new FormControl("", Validators.required),
+      end: new FormControl("", Validators.required),
+      summary: new FormControl("", [Validators.required, Validators.pattern(/^\S.*/), Validators.maxLength(140)]),
+      description: new FormControl(),
+      currency: new FormControl(this.defaultCurrencyCode, Validators.required),
+      online: new FormControl(true),
+      venueName: new FormControl(),
+      venueAddress1: new FormControl(),
+      venueAddress2: new FormControl(),
+      venueCity: new FormControl(),
+      venueRegion: new FormControl(),
+      venuePostCode: new FormControl(),
+      venueCountry: new FormControl(this.defaultCountryCode),
+      ticketName: new FormControl("", [Validators.required, Validators.pattern(/^\S.*/)]),
+      ticketQuantity: new FormControl("", Validators.required),
+      ticketFree: new FormControl(true),
+      ticketCost: new FormControl()
+    }, {validators: [futureStartValidator, endAfterStartValidator]}
+  )
+
+  venueCountry$!: Observable<any>
+ 
+  regions = signal(iso3166_2.country(this.defaultCountryCode)?.sub)
+
+  ngOnInit() {
+    this.venueCountry$ = this.eventForm.get("venueCountry")!.valueChanges
+    this.venueCountry$.subscribe(country => {
+      this.regions.set(iso3166_2.country(country)?.sub)
+    })
+  }
+
+  submitted = false;
+  @Output() eventPublished = new EventEmitter<boolean>();
+
+  async onSubmit() {
+    if (this.eventForm.valid) {
+      this.submitted = true;
+      const eventBallOrganizationId = "2517053640201"
+      const eventBallOrganizerId = "104041490841"
+      const res = await this.eventsService.postEntireEvent(this.eventForm.value, eventBallOrganizationId, eventBallOrganizerId)
+      this.eventPublished.emit(res)
+    }
+  }
+
 }
+
--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { CreateEventFormComponent } from './create-event-form/create-event-form.component';
+import { MatAnchor } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-event',
-  imports: [CreateEventFormComponent],
+  imports: [CreateEventFormComponent, RouterLink, MatAnchor],
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css'
 })
 export class CreateEventComponent {
+  eventPublishSuccessful = false
+  publishedEventId = ""
 
+  eventPublished(publishResponse: any) {
+    this.eventPublishSuccessful = publishResponse.published
+    this.publishedEventId = publishResponse.eventId
+  }
 }
