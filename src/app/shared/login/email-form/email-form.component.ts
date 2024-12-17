7f8a5c9c75da990a0092fed4713f037c6bbc51dd
George Bartram <george.bartram29@gmail.com>
2024-12-17 21:41:15 +0000

    adds user authentication using Firebase auth with angularfire

---
+++ src/app/shared/login/email-form/email-form.component.ts
@@ -0,0 +1,57 @@
+import { Component, input, output, signal, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-email-form',
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule],
+  templateUrl: './email-form.component.html',
+  styleUrl: './email-form.component.css'
+})
+export class EmailFormComponent {
+  emailForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+  })
+
+  submitted = signal(false)
+  signInClicked = signal(false)
+  emailAccountSubmitted = output<Object>()
+  signInError = input<any>()
+  signInErrorMessage = signal<String | null>(null)
+
+  onSubmit() {
+    this.submitted.set(true)
+    this.signInErrorMessage.set(null)
+    const userDetails = {
+      signIn: true,
+      email: this.emailForm.get("email")?.value,
+      password: this.emailForm.get("password")?.value,
+    }
+    if (this.signInClicked()) {
+      this.emailAccountSubmitted.emit(userDetails)
+    } else {
+      userDetails.signIn = false
+      this.emailAccountSubmitted.emit(userDetails)
+    }
+  }
+
+  onSignIn() {
+    this.signInClicked.set(true)
+  }
+
+  onSignUp() {
+    this.signInClicked.set(false)
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const formSignInError = changes['signInError']
+    if (formSignInError.currentValue) {
+      this.submitted.set(false)
+      const errorCode: string = formSignInError.currentValue.errorCode
+      const message = errorCode.split("/")[1].replaceAll("-", " ")
+      this.signInErrorMessage.set(`Error: ${message}`)
+    }
+  }
+}
--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog'; 
+import { MatSnackBar } from '@angular/material/snack-bar'; 
+import { LoginComponent } from '../login/login.component';
+import { Auth, User, user, getRedirectResult, GoogleAuthProvider } from '@angular/fire/auth';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
@@ -8,4 +13,50 @@ import { RouterLink } from '@angular/router';
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  private auth = inject(Auth);
+  user$ = user(this.auth);
+  userSubscription: Subscription;
+  currentUser: WritableSignal<User | null> = signal(null)
+
+  constructor(private loginDialog: MatDialog, private logoutSnackBar: MatSnackBar) {
+    this.userSubscription = this.user$.subscribe((user: User | null) => {
+      this.currentUser.set(user)
+    })
+  }
+
+  openLoginDialog() {
+    this.loginDialog.open(LoginComponent)
+  }
+
+  openlogoutSnackBar(message: string) {
+    this.logoutSnackBar.open(message, "OK", {
+      duration: 5000,
+    });
+  }
+
+  async signOut() {
+    try {
+      await this.auth.signOut()
+      this.openlogoutSnackBar("Signed out")
+    } catch (error: any) {
+      console.error('Error signing out: ', error)
+      this.openlogoutSnackBar(`Error signing out: ${error.message}`)
+    }
+  }
+
+  async ngOnInit() {
+    const result = await getRedirectResult(this.auth);
+    console.log(result);
+    if (result) {
+      const user = result.user;
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      console.log(result, user, credential, token);
+    }
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
+}
